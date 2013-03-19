05498208dbf56da2a1bdf0547737904dfb10de3f
Dominik Piatek <dominik.piatek@econsultancy.com>
2013-03-19 21:33:24 +0000

    Add watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,12 +15,20 @@ module.exports = function(grunt) {
       dist: {
         files: { 'css/main.css': 'css/main.scss' }
       }
+    },
+
+    watch: {
+      css: {
+        files: ['css/**/*.scss'],
+        tasks: ['sass']
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('concat', ['concat']);
   grunt.registerTask('s', ['sass']);
